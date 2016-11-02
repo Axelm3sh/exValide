cc422db95f715387b878a28d9da6a3512047e0d1
Axelm3sh <axel.m3sh@gmail.com>
2016-11-02 15:30:03 -0700

    Added Json Parser, Updated TSprite
    Json parser added, used for ease of creating objects such as sprites or
    animations, will extend functionality some more later, maybe pave the
    way for easy modding
    
    TSprite Added a bunch of stuff to the header file, putting the pavement
    down for the eventualty that this class will be used for 99.9% of
    practically everything that is being displayed.

--- exValide/src/Object/TSprite.cpp
+++ exValide/src/Object/TSprite.cpp
@@ -9,4 +9,24 @@ TSprite::TSprite()
 
 TSprite::~TSprite()
 {
+	ObjTexture.free();
+	rendererPtr = NULL;
+}
+
+void TSprite::InitSprite(SDL_Renderer * targetRenderer, bool isImage, std::string filePathOrText, SDL_Color color, int txtSize)
+{
+	rendererPtr = targetRenderer;
+
+	if(rendererPtr)
+	{
+		if (isImage)
+		{
+			ObjTexture.LoadFromFile(filePathOrText, color);
+		}
+		else
+		{
+			ObjTexture.LoadFromText(filePathOrText, color, txtSize);
+		}
+	}
+	
 }
--- exValide/src/Object/TSprite.h
+++ exValide/src/Object/TSprite.h
@@ -3,6 +3,7 @@
 
 #include "../Texture.h"
 #include "AObject.h"
+#include <vector>
 
 
 class TSprite :	public AObject
@@ -11,17 +12,47 @@ public:
 	TSprite();
 	~TSprite();
 
-	void InitSprite(SDL_Renderer* targetRenderer, bool isImage, );
+	struct AnimationData
+	{
+		std::string AnimationName;
+		//From a given sprite sheet, animations can be used by flipping through a frame range. //DEVELOPMENT
+		int maxFrameRange;
+		std::vector<SDL_Rect> SprFrameRects;
+	};
+
+	/*@brief Initialize sprite to be ready to displayed, Sets bounding box to entire image or text initially
+	@param targetRenderer the renderer to be used when sprite is loaded
+	@param isImage this bool sets if the sprite used is an Image or if it's text
+	@param filePathOrText used for file path to image or the text to be displayed as the sprite
+	@param color SDL_Color struct for keying out the image color or the text color to be used
+	@param txtSize ONLY USED WITH TEXT, DEFAULT 24pt
+	*/
+	void InitSprite(SDL_Renderer* targetRenderer, bool isImage, std::string filePathOrText, SDL_Color color, int txtSize = 24);
+
+	/* TODO - Parse data from json? easier to specify stuff perhaps
+	@brief Initialize an animation for a sprite from the current Sprite Image/Sheet
+	@param xStartPosition, used to determine the starting x position of the animation
+	@param yStartPosition, used to determine the starting y position of the animation
+	@param frameHeight, frameWidth, the height and width of a single frame to be used in animation
+	@param maxFrames, max number of frames this animation will have
+	@param AnimationName, The name of the animation to be stored for later reference if needed
+	*/
+	void InitSprAnim(int xStartPos, int yStartPos, int frameWidth, int frameHeight, int maxFrames, std::string AnimationName = "Anim_Default");
+
+	void SetFrame(int frame);
 
 protected:
 	Texture ObjTexture;
-	bool bIsAnimated; //True if sprite is animated, false if it sprite is not
-	int numSkins; //Number of alternate skins a sprite has
 
-	//Sprite Bounding Box, reflects Maximum edge of the sprite. Used for simple Collision Checks
+	//Sprite Bounding Box, reflects MAXIMUM size of the sprite. Used for simple Collision Checks
 	SDL_Rect SBBox;
 
-	SDL_Renderer* rendererPtr;
+	std::vector<AnimationData> FlipbookAnim;
+
+	bool bIsAnimated; //True if sprite is animated, false if it sprite is not //DEVELOPMENT
+	int numSkins; //Number of alternate skins a sprite has //DEVELOPMENT
+	
+	SDL_Renderer* rendererPtr; //Pointer to renderer, make sure this is set by calling InitSprite
 };
 
-#endif
+#endif /*TSPRITE_H*/
