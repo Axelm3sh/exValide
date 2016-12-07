84b0dd3525c3b0a1c53572ce49744a3ee88cbd45
Axelm3sh <axel.m3sh@gmail.com>
2016-12-07 12:59:35 -0800

    minor comments

--- exValide/src/FileDat/ResourceObject.h
+++ exValide/src/FileDat/ResourceObject.h
@@ -9,16 +9,16 @@ using json = nlohmann::json;
 enum RESOURCE_TYPE //Can we map strings to enums?
 {
 	INVALID,
-	ALIAS,
-	OBJECT,
-	ANIMATION
+	RESOURCE, //Resource File, <fileName.robj, pathName> 
+	OBJECT,	  //Resource File Object, <objectName, easyName>
+	ANIMATION //Animation Data File, <see AnimationOject, too many values>
 	//Todo, extend for more Resource types
 };
 
 struct PathObject
 {
 	std::string pathToObject; //Relative Path
-	std::string easyName;
+	std::string easyName; //Nickname of object for reference
 };
 
 struct AnimationObject
@@ -38,17 +38,14 @@ class ResourceObject
 {
 public:
 	
-
-
 	ResourceObject(RESOURCE_TYPE resType);
 	~ResourceObject();
 
-	void Serialize();
-	json Deserialize();
+	void Serialize(map<string, string> mMap); //to-string json via map
+	json Deserialize(); //from-string json
 	
 private:
 	RESOURCE_TYPE resourceType;
-
 	
 };
 
